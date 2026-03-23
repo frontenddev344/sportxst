fbfc2ce92fc5bed3d6cb034e2c04d2d6a2f03dcc
Dev Avology <devavology12@gmail.com>
2026-03-23 18:28:55 +0530

    first commi

---
+++ assets/js/index.js
@@ -0,0 +1,51 @@
+// Toggle Menu Functionality Start
+
+function openMenu() {
+  document.getElementById("sidebar").classList.add("active");
+}
+
+function closeMenu() {
+  document.getElementById("sidebar").classList.remove("active");
+}
+// Toggle Menu Functionality End
+
+  // Scroll to next section Start
+function scrollToNext() {
+  document.getElementById("next-section").scrollIntoView({
+      behavior: "smooth"
+  });
+}
+  // Scroll to next section End
+  // AOS JS Start
+  AOS.init({
+    duration: 1200,
+  });
+  // AOS JS End
+  
+  window.addEventListener("load", function () {
+    if (window.location.hash === "#contact_page") {
+      const section = document.getElementById("contact_page");
+      const offset = 150; 
+  
+      if (section) {
+        const y = section.getBoundingClientRect().top + window.pageYOffset - offset;
+  
+        window.scrollTo({
+          top: y,
+          behavior: "smooth"
+        });
+      }
+    }
+  });
+
+
+
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header-container");
+
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
